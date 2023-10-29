8946dd2609244233964795eae7b2335fac9bb063
kannan-ravi <kannanravindran00kr@gmail.com>
2023-10-29 15:52:21 +0530

    Updated cors options

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import cors from 'cors';
+import cors from "cors";
 import connectDB from "./config/dbConnection.js";
+import corsOptions from "./config/corsOptions.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 connectDB();
+
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 mongoose.connection.once("open", () => {
   console.log("Connected to Database");
