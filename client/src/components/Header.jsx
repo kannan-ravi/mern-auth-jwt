ab109d0534616fc585bd5433d837603c459d07ee
kannan-ravi <kannanravindran00kr@gmail.com>
2023-10-29 14:30:41 +0530

    Header componenet created

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <header className="bg-slate-200">
+      <div className="container mx-auto flex justify-between items-center p-3">
+        <Link to="/">
+          <h1 className="font-bold text-base">KANNA APP</h1>
+        </Link>
+        <div className="flex items-center gap-5 text-xs">
+          <Link
+            to="/"
+            className="font-medium hover:text-blue-500  duration-300 transition-colors"
+          >
+            Home
+          </Link>
+          <Link
+            to="/about"
+            className="font-medium hover:text-blue-500 duration-300 transition-colors"
+          >
+            About
+          </Link>
+          <Link
+            to="/login"
+            className="font-medium hover:text-blue-500 duration-300 transition-colors"
+          >
+            Login
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
