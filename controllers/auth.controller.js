22596bf35a0e5a8df1e541dc8c3a8dd5f50f98dd
kannan-ravi <kannanravindran00kr@gmail.com>
2023-11-08 00:11:02 +0530

    Implemented Google OAuth for backend.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -39,6 +39,43 @@ const singIn = async (req, res, next) => {
   }
 };
 
-const googleAuth = async (req, res, next) => {};
+const googleAuth = async (req, res, next) => {
+  try {
+    const valideUser = await UserModel.findOne({ email: req.body.email });
+    if (valideUser) {
+      const token = jwt.sign({ id: valideUser._id }, process.env.JWT_SECRET);
+      const { password: hashedPassword, ...rest } = valideUser._doc;
+      const expireDate = new Date(Date.now() + 24 * 60 * 60 * 1000);
+      res
+        .cookie("token", token, { httpOnly: true, expires: expireDate })
+        .status(200)
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 14);
+      const username =
+        req.body.name.split(" ").join("").toLowerCase() +
+        Math.random().toString(36).slice(-8);
+      const newUser = new UserModel({
+        username: username,
+        email: req.body.email,
+        password: hashedPassword,
+        profilePicture: req.body.photo,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: hashedPassword2, ...rest } = newUser._doc;
+      const expireDate = new Date(Date.now() + 24 * 60 * 60 * 1000);
+      res
+        .cookie("token", token, { httpOnly: true, expires: expireDate })
+        .status(200)
+        .json(rest);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 export default { signUp, singIn, googleAuth };
