62b889edce77101f626a8a4cea46c30e285e4fff
kannan-ravi <kannanravindran00kr@gmail.com>
2023-11-09 20:51:04 +0530

    Implemented Private Router & used user content.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className="bg-slate-200">
       <div className="container mx-auto flex justify-between items-center p-3">
@@ -21,10 +24,18 @@ const Header = () => {
             About
           </Link>
           <Link
-            to="/sign-in"
+            to="/profile"
             className="font-medium hover:text-blue-500 duration-300 transition-colors"
           >
-            Sign In
+            {currentUser ? (
+              <img
+                src={currentUser.profilePicture}
+                alt="Profile Picture"
+                className="h-7 w-7 rounded-full object-cover"
+              />
+            ) : (
+              "Sign In"
+            )}
           </Link>
         </div>
       </div>
