5b0969562690caec78bf685fc21f5322abc1c59f
kannan-ravi <kannanravindran00kr@gmail.com>
2023-11-11 17:43:45 +0530

    home & about page completed

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-700 text-center uppercase">
+        About
+      </h1>
+      <p className="mb-4 text-slate-700 text-center">---</p>
+    </div>
+  );
 }
 
-export default About
+export default About;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-700 text-center uppercase">
+        Welcome
+      </h1>
+      <p className="mb-4 text-slate-700 text-center">---</p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
