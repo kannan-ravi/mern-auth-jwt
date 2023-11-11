e0af692128a5dabf2d011903ef6019ff69b5d0ce
kannan-ravi <kannanravindran00kr@gmail.com>
2023-11-11 18:09:02 +0530

    Working on Deployment error

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "index.html"));
 });
